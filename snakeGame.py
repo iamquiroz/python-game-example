3a0fc54548e8f18aa27ae38f090599c77d811901
Josh W <sdkjqg@gmail.com>
2022-02-21 21:24:54 -0800

    snake game added

---
+++ snakeGame.py
@@ -0,0 +1,26 @@
+from re import I
+import pygame
+import time
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+
+dis_width = 600
+dis_height = 400
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+
+clock = pygame.time.Clock()
+
+snake_block=10
+snake_speed=15
+
